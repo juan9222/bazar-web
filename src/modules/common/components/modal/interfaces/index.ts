f8a8c2587ba98bb4e2f374dfad7e9f5e2ac7de65
Cristopher Paniagua <cpaniagua@cafetosoftware.com>
2022-10-25 22:51:21 -0500

    Adds UI Component for Modal Views

---
+++ src/modules/common/components/modal/interfaces/index.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+export interface IModalProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+  title: string;
+  width?: string;
+  maxWidth?: string;
+  height?: string;
+  maxHeight?: string;
+  closed: boolean;
+  continueDisabled: boolean;
+  onClose: (event: React.MouseEvent) => void;
+  onContinue: (event: React.MouseEvent) => void;
+}
